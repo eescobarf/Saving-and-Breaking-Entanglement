154bff224d0161e80ab02b5df58d0864a94bca18
eescobarf <enriquees.fdez@gmail.com>
2025-06-16 11:50:46 +0200

    Development of is_es_qubit function

--- Code/utils.py
+++ Code/utils.py
@@ -1,4 +1,4 @@
-from qiskit.quantum_info import DensityMatrix
+from qiskit.quantum_info import DensityMatrix, Pauli, SuperOp, Operator, Choi, PTM
 import numpy as np
 
 def is_ppt(choi):
@@ -19,3 +19,35 @@ def is_eppt(choi, app):
         choi = choi.compose(choi)
     # Check if the resulting Choi matrix is PPT
     return is_ppt(choi)
+
+
+
+def is_es_qubit(choi):
+    """Check if a qubit channel is ES (Esntanglement Saving)"""
+    # First we compute the Pauli Transfer Matrix of the state and then extract the relevant sub,atrix and vector
+    ptm = PTM(SuperOp(choi)).data
+    M = ptm[1:4, 1:4]
+    c = ptm[1:4, 0]
+
+    # Check determinant, as in the qubit case this would mean that the channel es EB direfctly.
+    if np.linalg.det(M) == 0:
+        return False
+
+    # Check if the channel is unital, i.e if c = 0
+    unital = np.array_equal(c, np.zeros(3))
+
+    # Divide in unital and non unital case
+    if unital:       
+        eigenvalues = np.linalg.eigvals(M)
+        return any(eig == -1 for eig in eigenvalues)
+    else:
+        # For the non unital case, we check if (M - I) n = -c has a solution with ||n|| = 1 (pure state)
+        A = M - np.eye(3)
+        b = -c
+        # Solve A @ n = b
+        n, residuals, rank, _ = np.linalg.lstsq(A, b, rcond=None)
+        if residuals.size == 0 or np.all(residuals == 0):  # Exact solution exists
+            # Check if n is a unit vector 
+            if np.isclose(np.linalg.norm(n), 1):
+                return True
+        return False
